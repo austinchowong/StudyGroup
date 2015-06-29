a4d971bc2e3b4c726bb8e4d958489490c4116fac
Austin <austin.cho.wong@gmail.com>
2015-06-29 19:49:18 -0400

    add user microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,4 +16,13 @@ User.create!(name:  "Austin",
                password_confirmation: password,
                activated: true,
                activated_at: Time.zone.now)
+
 end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
+
